f491f8369a154fc4b3fe69999109a9e191348151
Atul Chauhan <atul.chauhan@publicissapient.com>
2020-07-08 23:49:12 +0530

    intial code for discovery service

---
+++ src/test/java/com/discovery/contactdiscoveryservice/ContactDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.contactdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
